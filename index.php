4ddb44d168436b9ef7e4e51bf5eef696049f2a7d
LCR3tro <lucassilvacerqueira29@gmail.com>
2026-06-21 16:32:39 -0300

    inicio de Projeto

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HitBox | Seu Mundo de Games</title>
+    <link rel="icon" href="Css\hit.png">
+    <link rel="stylesheet" href="Css\style.css">
+
+</head>
+
+<body>
+    <header>
+        <h1 style="margin-left: 120px;margin-top: 30px;">HitBox</h1>
+    <img src="Css\hit.png" style="margin-left: 10px; margin-top: -60px;" height="90" width="90" alt="Logo HitBox">
+     
+    <nav class="menu-superior">
+    <ul>
+      <li><a href="#">Nóticias</a></li>
+      <li><a href="#">Games</a></li>
+      <li><a href="#">E-Sports</a></li>
+      <li><a href="#">Cultura Pop</a></li>
+    </ul>
+  </nav>
+
+    </header>
+
+
+ 
+
+
+</body>
